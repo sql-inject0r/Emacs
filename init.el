4ede5937bb13855ea35972715150facd42c6c8ea
sql-inject0r <Adityaa_Tik00@protonmail.com>
2021-11-11 12:59:39 +0530

    First Commit

---
+++ init.el
@@ -0,0 +1,34 @@
+;;melpa package manager -
+
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+;;use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;import the file
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doom-acario-dark))
+ '(custom-safe-themes
+   '("da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" default))
+ '(package-selected-packages
+   '(slime-company slime company-shell company-irony company-c-headers flycheck-clang-analyzer company-jedi treemacs swiper company switch-window org-bullets popup-kill-ring rainbow-delimiters rainbow-mode beacon hungry-delete fancy-battery dashboard projectile helm doom-modeline which-key doom-themes use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
